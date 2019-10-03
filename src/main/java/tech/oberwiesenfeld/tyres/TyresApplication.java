e6ea3cb860e777a5ad3dc6f44de4dbd47d7bd853
oberwiesenfeld <oberwiesenfeld@users.noreply.github.com>
2019-10-03 16:06:51 +0200

    bootstrap spring and maven environment

---
+++ src/main/java/tech/oberwiesenfeld/tyres/TyresApplication.java
@@ -0,0 +1,13 @@
+package tech.oberwiesenfeld.tyres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyresApplication.class, args);
+	}
+
+}
