74bdc1e91e0ef8d8b73f8d1378690711bab17fed
oberwiesenfeld <oberwiesenfeld@users.noreply.github.com>
2019-10-04 15:47:24 +0200

    clean code topic

--- src/main/java/tech/oberwiesenfeld/tyres/TyreCalculateExtent.java
+++ src/main/java/tech/oberwiesenfeld/tyres/TyreCalculateExtent.java
@@ -2,20 +2,24 @@ package tech.oberwiesenfeld.tyres;
 
 import java.math.BigDecimal;
 
-import static java.lang.Math.PI;
 import static java.math.BigDecimal.valueOf;
 
 public class TyreCalculateExtent {
 
     private static final BigDecimal TWO = valueOf(2.0);
+    private static final BigDecimal PI = valueOf(Math.PI);
 
+    @SuppressWarnings("UnnecessaryLocalVariable")
     public BigDecimal calculateExtent(Tyre tyre, TyreUnits tyreUnit) {
+        BigDecimal twoTimesTyreHeight = calculateTwoTimesTyreHeight(tyre, tyreUnit);
+        BigDecimal wheelAndTyreDiameter = twoTimesTyreHeight.add(tyre.getWheelWidth().getValue(tyreUnit));
+        BigDecimal extent = wheelAndTyreDiameter.multiply(PI);
+        return extent;
+    }
+
+    private BigDecimal calculateTwoTimesTyreHeight(Tyre tyre, TyreUnits tyreUnit) {
         BigDecimal tyreWidth = tyre.getTyreWidth().getValue(tyreUnit);
         BigDecimal tyreHeight = tyreWidth.multiply(tyre.getTyreAspectRatio());
-        BigDecimal extent = tyreHeight.multiply(TWO);
-
-        extent = extent.add(tyre.getWheelWidth().getValue(tyreUnit));
-        extent = extent.multiply(valueOf(PI));
-        return extent;
+        return tyreHeight.multiply(TWO);
     }
 }
