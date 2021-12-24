75898bf6c43978417ea0367d49aca7d63342343c
oberwiesenfeld <48629253+oberwiesenfeld@users.noreply.github.com>
2021-12-24 15:41:54 +0100

    Update TyreCalculateExtentTest.java

--- src/test/java/tech/oberwiesenfeld/tyres/TyreCalculateExtentTest.java
+++ src/test/java/tech/oberwiesenfeld/tyres/TyreCalculateExtentTest.java
@@ -1,6 +1,5 @@
 package tech.oberwiesenfeld.tyres;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
